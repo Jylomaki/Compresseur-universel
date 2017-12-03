7185b3e003ea60a17cacc24449f3cef845c7db88
Jylomaki <gaby.denoix@gmail.com>
2017-12-03 17:40:08 +0100

    updating the script

--- make.sh
+++ make.sh
@@ -1,16 +1,14 @@
 
 g++ -g -std=c++11  main.cpp ImageBase.cpp -o  main
 
- ./main images/baboon.ppm images/baboon_out.ppm 256 0 1
+./main c images/baboon.ppm 
+./main c images/14_dirtypair.ppm 
+./main c images/chaperon.ppm 
+./main c images/15_kodim03_Hats.ppm 
+./main c images/14_Perroquet.ppm 
+./main c images/Lena.ppm 
+./main c images/tortue.ppm 
 
- ./main images/14_dirtypair.ppm images/14_dirtypair_out.ppm 256 0 1
- ./main images/15_CT_18330_70.ppm images/15_CT_18330_70_out.ppm 256 0 1
- ./main images/chaperon.ppm images/chaperon_ou.ppm 256 0 1
- ./main images/15_kodim03_Hats.ppm images/15_kodim03_Hats_out.ppm 256 0 1
- ./main images/14_Perroquet.ppm images/14_Perroquet_out.ppm 256 0 1
- ./main images/Lena.ppm images/Lena_out.ppm 256 0 1
- ./main images/tortue.ppm images/tortue_out.ppm 256 0 1
-
-
-14_Medical.pgm
-15_Girafes.pgm 
+./main c images/15_CT_18330_70.pgm 
+./main c images/14_Medical.pgm
+./main c images/15_Girafes.pgm 
